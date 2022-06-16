4b1274e0c3c11743392dbcfa09c98ce40ce42c6b
Chandan Patil <chandan.p11@gmail.com>
2022-06-16 21:02:39 +0530

    added useSelector for user redux reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,34 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { CartContext } from "../../contexts/cart.context";
+// import { UserContext } from "../../contexts/user.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 // import "./navigation.styles.jsx";
+
 import {
   NavigationContainer,
   LogoContainer,
   NavLinks,
   NavLink,
 } from "./navigation.styles";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { CartContext } from "../../contexts/cart.context";
+
+
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  // const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser)
+  
   const { isCartOpen } = useContext(CartContext);
+  
+  
   // const { currentUser, setCurrentUser } = useContext(UserContext);
   // console.log(currentUser);
 
@@ -39,9 +51,8 @@ const Navigation = () => {
           <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
             <NavLink as="span" onClick={signOutUser}>
-              {/* <span className="nav-link" onClick={signOutUser}> */} 
-              SIGN-OUT{" "}
-              {/* </span> */}
+              {/* <span className="nav-link" onClick={signOutUser}> */}
+              SIGN-OUT {/* </span> */}
             </NavLink>
           ) : (
             <NavLink to="/auth">SIGN IN</NavLink>
