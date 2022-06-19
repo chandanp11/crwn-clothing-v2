01f4cfe960c92cc7ac5f17c776eb35cf04044721
Chandan Patil <chandan.p11@gmail.com>
2022-06-19 09:20:54 +0530

    converted category context to redux

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,23 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+// sample code for logger if we have to write our own
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("Payload: ", action.payload);
+  console.log("current State: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+// const middleWare = [loggerMiddleware];
+
 const middleWare = [logger];
 const componsedEnhancer = compose(applyMiddleware(...middleWare));
 export const store = createStore(rootReducer, undefined, componsedEnhancer);
